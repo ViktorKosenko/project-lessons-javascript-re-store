a7749a588f5f8d4bea6da609dd7bb5e5e5eca67c
Viktor <kosenkovv89@gmail.com>
2020-09-15 17:50:36 +0300

    to138

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import { HomePage, CartPage} from '../pages';
+
 import './app.css';
 
-const App = () => {
+const App = () => {;
     return (
-        <div>App</div>
-    )
-}
+        <Switch>
+            <Route
+            path="/"
+            component={HomePage}
+            exact />
+            <Route
+            path="/cart"
+            component={CartPage} />   
+        </Switch>
+    );
+};
 
 export default App;
--- src/components/hoc/with-bookstore-service.js
+++ src/components/hoc/with-bookstore-service.js
@@ -1,4 +1,4 @@
-import React from 'React';
+import React from 'react';
 import { BookstoreServiceConsumer } from '../bookstore-service-context';
 
 const withBookstoreService = () => (Wrapped) => {
