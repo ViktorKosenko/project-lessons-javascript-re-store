6dd555261db162610e36053549aed7b8035fb153
Viktor <kosenkovv89@gmail.com>
2020-09-16 10:01:51 +0300

    adding styles

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -4,11 +4,15 @@ export default class BookstoreService {
             { 
                 id: 1,
                 title: 'Production-Ready Microservices',
-                author: 'Susan J. Fowler'},
+                author: 'Susan J. Fowler',
+                price: 32,
+                coverImage: 'https://images-na.ssl-images-amazon.com/images/I/51oxXEG6TRL._SX379_BO1,204,203,200_.jpg'},
             { 
                 id: 2,
                 title: 'Release It!',
-                author: 'Michael T. Nygard'}
+                author: 'Michael T. Nygard',
+                price: 45,
+                coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41nMZPJdhsL._SX415_BO1,204,203,200_.jpg'}
         ];
     }
 }
